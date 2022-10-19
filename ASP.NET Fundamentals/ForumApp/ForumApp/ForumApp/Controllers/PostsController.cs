d837f42a8a560364c248d954f17a0626ba63b381
Boris <borisgeorgievdg@gmail.com>
2022-10-19 18:06:23 +0300

    Forum app

---
+++ ASP.NET Fundamentals/ForumApp/ForumApp/ForumApp/Controllers/PostsController.cs
@@ -0,0 +1,87 @@
+﻿using ForumApp.Data;
+using ForumApp.Data.Entities;
+using ForumApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForumApp.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly ForumAppDbContext Data;
+
+        public PostsController(ForumAppDbContext data)
+        {
+            this.Data = data;
+        }
+
+        public IActionResult All()
+        {
+            var posts =
+                Data
+                .Posts
+                .Select(x => new PostViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Content = x.Content,
+                })
+                .ToList();
+
+            return View(posts);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(PostFormModel model)
+        {
+            var post = new Post
+            {
+                Title = model.Title,
+                Content = model.Content,
+            };
+
+            this.Data.Posts.Add(post);
+            this.Data.SaveChanges();
+
+            return RedirectToAction("All");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var post = this.Data.Posts.Find(id);
+
+            return View(new PostFormModel
+            {
+                Title = post.Title,
+                Content = post.Content,
+            });
+        }
+
+        [HttpPost]
+        public IActionResult Edit(PostFormModel model, int id)
+        {
+            var post = this.Data.Posts.Find(id);
+            post.Title = model.Title;
+            post.Content = model.Content;
+
+            this.Data.SaveChanges();
+
+            return RedirectToAction("All");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var post = this.Data.Posts.Find(id);
+
+            this.Data.Posts.Remove(post);
+            this.Data.SaveChanges();
+
+            return RedirectToAction("All");
+        }
+    }
+}
---
+++ ASP.NET Fundamentals/ForumApp/ForumApp/ForumApp/Data/ForumAppDbContext.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ForumApp.Data.Entities;
+
+namespace ForumApp.Data
+{
+    public class ForumAppDbContext : DbContext
+    {
+        private Post FirstPost { get; set; }
+
+        private Post SecondPost { get; set; }
+
+        private Post ThirdPost { get; set; }
+
+        public ForumAppDbContext(DbContextOptions<ForumAppDbContext> options)
+            : base(options)
+        {
+            this.Database.Migrate();
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            //builder
+            //    .Entity<Post>()
+            //    .HasMany(p => p.PostAnswers)
+            //    .WithOne(r => r.Post)
+            //    .OnDelete(DeleteBehavior.Restrict);
+
+            SeedPosts();
+            builder
+                .Entity<Post>()
+                .HasData(this.FirstPost,
+                 this.SecondPost,
+                this.ThirdPost);
+
+            base.OnModelCreating(builder);
+        }
+
+        private void SeedPosts()
+        {
+            this.FirstPost = new Post
+            {
+                Id = 1,
+                Title = "My First Post",
+                Content = "My first post will be about performing CRUD operations in MVC. Its so much fun!"
+            };
+
+            this.SecondPost = new Post
+            {
+                Id = 2,
+                Title = "My Second Post",
+                Content = "This is my second post CRUD operations in MVC are getting more and more intresting!"
+            };
+
+            this.ThirdPost = new Post
+            {
+                Id = 3,
+                Title = "My Third Post",
+                Content = "Hello there! Im getting better and better with the CRUD operations in MVC. Stay tuned!"
+            };
+        }
+    }
+}
